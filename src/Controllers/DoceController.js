4af475854285688348d8e7d8a3b86cc635edabb5
Marco Medeiros <marco.a.medeiros6@aluno.senai.br>
2024-06-28 17:19:54 -0300

    loja doces provinha

--- src/Controllers/DoceController.js
+++ src/Controllers/DoceController.js
@@ -1,71 +1,73 @@
-import CategoriaModel from "../Models/CategoriaModel.js";
+import DoceModel from "../Models/DoceModel.js"
 
-class CategoriaController{
+class DoceController{
     constructor(){
 
     }
 
     create(req,res){
-        const nome_categoria = req.body.nome_categoria
-        CategoriaModel.create(nome_categoria).then(
+        const nome_doce = req.body.nome_doce
+        const id_categoria = req.body.id_categoria
+        DoceModel.create(nome_doce,id_categoria).then(
             resposta =>{
-                console.debug("Cadastrando uma categoria")
+                console.debug("Cadastrando um doce")
                 res.status(resposta[0]).json(resposta[1])
             }
         ).catch(
             resposta =>{
                 console.debug(resposta)
-                console.debug("Erro cadastrando uma categoria")
+                console.debug("Erro cadastrando um doce")
                 res.status(resposta[0]).json(resposta[1])
             }
         )
     }
     read(req,res){
-        CategoriaModel.read().then(
+        DoceModel.read().then(
             resposta =>{
-                console.debug("Exibindo categorias")
+                console.debug("Exibindo doces")
                 res.status(resposta[0]).json(resposta[1])
             }
         ).catch(
             resposta =>{
                 console.debug(resposta)
-                console.debug("Erro Exibindo categorias")
+                console.debug("Erro Exibindo doces")
                 res.status(resposta[0]).json(resposta[1])
             }
         )
     }
     update(req,res){
-        const id_categoria = req.params.id_categoria
-        const nome_categoria = req.body.nome_categoria
-        CategoriaModel.update(id_categoria,nome_categoria).then(
+        const id_doce = req.params.id_doce
+        const nome_doce = req.body.nome_doce
+        const id_categoria = req.body.id_categoria
+        DoceModel.update(id_doce,nome_doce,id_categoria).then(
             resposta =>{
-                console.debug("Atualizando categoria")
+                console.debug("Atualizando doce")
                 res.status(resposta[0]).json(resposta[1])
             }
         ).catch(
             resposta =>{
                 console.debug(resposta)
-                console.debug("Erro Atualizando categoria")
+                console.debug("Erro Atualizando doce")
                 res.status(resposta[0]).json(resposta[1])
             }
         )
     }
 
     delete(req,res){
-        const id_categoria = req.params.id_categoria
-        CategoriaModel.delete(id_categoria).then(
+        const id_doce = req.params.id_doce
+        DoceModel.delete(id_doce).then(
             resposta =>{
-                console.debug("Deletando categoria")
+                console.debug("Deletando doce")
                 res.status(resposta[0]).json(resposta[1])
             }
         ).catch(
             resposta =>{
                 console.debug(resposta)
-                console.debug("Erro Deletando categoria")
+                console.debug("Erro Deletando doce")
                 res.status(resposta[0]).json(resposta[1])
             }
         )
     }
 }
 
-export default new CategoriaController()
+export default new DoceController()
